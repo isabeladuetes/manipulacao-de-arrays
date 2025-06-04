ab0282386a7d82a0d61bc262705687a9cc2865a7
isabeladuetes <isabela.duetes@aluno.senai.br>
2025-06-04 15:08:21 -0300

    feat: Separa um relatório com apenas a primeira semana de férias

---
+++ atividade13.js
@@ -0,0 +1,13 @@
+let diasFaltas = [
+    "Carlos – Segunda",
+    "Carlos – Terça",
+    "Carlos – Quarta",
+    "Carlos – Quinta",
+    "Carlos – Sexta",
+    "Carlos – Segunda (semana 2)",
+    "Carlos – Terça (semana 2)",
+    "Carlos – Quarta (semana 2)"
+  ];
+
+// Separando um relatório com apenas a primeira semana de férias
+  console.log("Um relatório com apenas a primeira semana de férias (índices 0 a 5):", diasFaltas.slice(0, 5)); 
