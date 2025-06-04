2908813b0443db35ac0a3f490cccea7e0a1ea98d
isabeladuetes <isabela.duetes@aluno.senai.br>
2025-06-04 14:57:31 -0300

    feat: Tranforma lista de ingredientes em uma única frase

---
+++ atividade11.js
@@ -0,0 +1,10 @@
+let ingredientes = [
+    "farinha de trigo",
+    "fermento",
+    "leite",
+    "ovos",
+    "açúcar",
+    "manteiga"
+  ];
+
+console.log("Todos os ingredientes em uma frase:", ingredientes.join(', ')); 
