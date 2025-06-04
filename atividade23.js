19494b2caf14717de4365e90a31546cafca9a4df
isabeladuetes <isabela.duetes@aluno.senai.br>
2025-06-04 16:06:56 -0300

    feat: Organiza os candidatos em ordem alfabética

---
+++ atividade23.js
@@ -0,0 +1,16 @@
+let candidatos = [
+    "Marcos",
+    "Aline",
+    "Carla",
+    "João",
+    "Fernanda",
+    "Lucas",
+    "Bruno",
+    "Isabela"
+  ];
+
+  
+// Organizando os candidatos para a entrevista em ordem alfabética
+let conditatosOrdenados = candidatos.sort();
+
+console.log("Candidatos ordenados:", conditatosOrdenados);
