c29c054244e031f9c26433e50ad3178126f973d9
isabeladuetes <isabela.duetes@aluno.senai.br>
2025-06-04 15:35:14 -0300

    feat: Combina dois arrays em um catálogo

---
+++ atividade18.js
@@ -0,0 +1,16 @@
+let hardware = [
+    "processador i7",
+    "placa-mãe",
+    "memória RAM 16GB",
+    "SSD 1TB"
+  ];
+  
+  let perifericos = [
+    "teclado gamer",
+    "mouse ótico",
+    "webcam HD",
+    "fone com microfone"
+  ];
+
+// Juntado as duas listas
+console.log("Usuários combinados:", hardware.concat(perifericos));
