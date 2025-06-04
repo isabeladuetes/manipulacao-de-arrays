b1957fe667853e6ee7b5d9e9ddc6025db148f1c3
isabeladuetes <isabela.duetes@aluno.senai.br>
2025-06-04 15:13:04 -0300

    feat: Separe os artigos mais recentes

---
+++ atividade14.js
@@ -0,0 +1,13 @@
+let artigosBlog = [
+    "Como usar Docker com Node.js",
+    "5 erros comuns em JavaScript",
+    "Introdução ao TypeScript",
+    "Clean Code na prática",
+    "Design Patterns úteis",
+    "Refatoração de código",
+    "Boas práticas de API REST",
+    "Segurança em aplicações web"
+  ];
+
+// Separando os 4 artigos mais recentes da semana
+console.log("Os 4 artigos mais recentes da semana (índices 0 a 4):", artigosBlog.slice(0, 4)); 
