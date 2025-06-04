db38035dce7852c6a0aafe1b0ba7144812e3f7be
isabeladuetes <isabela.duetes@aluno.senai.br>
2025-06-04 16:19:00 -0300

    feat: Inverta a sequência dos arrays chamados históricos

---
+++ atividade26.js
@@ -0,0 +1,13 @@
+let historico = [
+    "login",
+    "painel",
+    "relatórios",
+    "detalhes do cliente",
+    "editar dados",
+    "configurações",
+    "logout"
+  ];
+
+// Invertendo o histórico
+  let historicoInvertido = historico.reverse()
+  console.log(historicoInvertido);
