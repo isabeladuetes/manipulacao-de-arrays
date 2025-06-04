43ead6bfe07a96dbce77f648f3ab151b43e7d0cd
isabeladuetes <isabela.duetes@aluno.senai.br>
2025-06-04 14:47:10 -0300

    feat: Busca na lista de presença uma pessoa

---
+++ atividade09.js
@@ -0,0 +1,10 @@
+let presenca = [
+    "João Lima",
+    "Carla Nunes",
+    "Amanda Souza",
+    "Bruno Ribeiro",
+    "Fernanda Sales"
+  ];
+
+// Buscando se uma pessoa está na lista de presença
+console.log(`Na lista de presença inclui 'Amanda Souza?' ${presenca.includes("Amanda Souza")}`);
