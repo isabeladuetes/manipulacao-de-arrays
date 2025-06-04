4dc1ed99f92ae1c80b5f8648bf29967c4e87adaa
isabeladuetes <isabela.duetes@aluno.senai.br>
2025-06-04 15:32:14 -0300

    feat: Combina dois arrays

---
+++ atividade17.js
@@ -0,0 +1,16 @@
+let usuariosSistemaA = [
+    "aline.souza",
+    "maria.ferreira",
+    "carlos.lima",
+    "ana.martins"
+  ];
+  
+  let usuariosSistemaB = [
+    "joao.silva",
+    "paulo.pereira",
+    "fernanda.alves",
+    "marcelo.cardoso"
+  ];
+  
+// Juntado as duas listas de usuários
+console.log("Usuários combinados:", usuariosSistemaA.concat(usuariosSistemaB));
