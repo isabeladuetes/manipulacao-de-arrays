8f782933a06facb345dc0cb9cc65ae2f4c18b9e6
isabeladuetes <isabela.duetes@aluno.senai.br>
2025-06-04 16:11:00 -0300

    feat: Ordena os números em ordem decrescente

---
+++ atividade24.js
@@ -0,0 +1,5 @@
+let vendas = [3500, 1800, 4200, 3900, 5000, 1200, 2900, 4700];
+
+// Ordenando os números de forma decrescente
+let numerosOrdenadosDecrescentes = vendas.sort((a, b) => b - a);
+console.log("Números de vendas em ordem decrescente:", numerosOrdenadosDecrescentes);
