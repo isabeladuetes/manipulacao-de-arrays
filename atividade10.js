3b34608a6b41e00daa37fea4536add4ff659502f
isabeladuetes <isabela.duetes@aluno.senai.br>
2025-06-04 14:51:51 -0300

    feat: Busca se um produto tem estoque

---
+++ atividade10.js
@@ -0,0 +1,10 @@
+let estoque = [
+    "mesa para PC",
+    "cadeira gamer",
+    "monitor 27 polegadas",
+    "hub USB",
+    "mouse sem fio"
+  ];
+
+// Buscando se um produto está em estoque
+console.log(`No estoque inclui 'cadeira gamer?' ${estoque.includes("cadeira gamer")}`);
